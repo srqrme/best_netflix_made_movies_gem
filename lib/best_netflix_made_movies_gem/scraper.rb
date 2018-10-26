8b6efe5ac85bd9b57f4592d3892e4a010d579475
Sharmaine Requerme <srqrme@gmail.com>
2018-10-26 17:21:46 +0000

    assigned title, rank, and movie_url attributes to #scrape_movie_index in scraper.rb file

--- lib/best_netflix_made_movies_gem/scraper.rb
+++ lib/best_netflix_made_movies_gem/scraper.rb
@@ -8,6 +8,9 @@ class BestNetflixMadeMoviesGem::Scraper
     netflix_movies_doc = Nokogiri::HTML(open("https://editorial.rottentomatoes.com/guide/best-netflix-movies-to-watch-right-now/"))
     movies = []
     netflix_movies_doc.css(".row.countdown-item").each do |row|
+      title = row.css(".article_movie_title h2 a").text
+      rank = row.css(".countdown-index").text
+      movie_url = row.css("a.article_movie_poster").attribute("href").value
   end
 
 end
