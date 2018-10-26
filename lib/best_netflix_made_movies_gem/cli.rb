603aca43855bececd8ad010c4b950bd9b5335b44
Sharmaine Requerme <srqrme@gmail.com>
2018-10-26 16:57:19 +0000

    created BestNetflixMadeMoviesGem::CLI class and added #start method. Added code to bin/bestNetflixMadeMovies file to instantiate CLI class and call #start to start up the program

---
+++ lib/best_netflix_made_movies_gem/cli.rb
@@ -0,0 +1,13 @@
+#CLI Controller
+require 'pry'
+
+class BestNetflixMadeMoviesGem::CLI
+  def start
+    puts ""
+    puts "Hello and Welcome to the Best Netflix Made Movies Gem:"
+    puts "A guide to the best Netflix made movies according to Rotten Tomatoes"
+    puts ""
+    menu
+  end
+
+end
