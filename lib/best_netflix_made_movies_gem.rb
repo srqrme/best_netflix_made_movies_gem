add013807369c257f92374822740ac4710537ad2
Sharmaine Requerme <srqrme@gmail.com>
2018-10-26 16:48:57 +0000

    created cli.rb, movie.rb, and scraper.rb files. Added file requirements to bin/bestNetflixMadeMovies and best_netflix_made_movies.rb

--- lib/best_netflix_made_movies_gem.rb
+++ lib/best_netflix_made_movies_gem.rb
@@ -1,4 +1,8 @@
-require "best_netflix_made_movies_gem/version"
+require_relative "./best_netflix_made_movies_gem/version"
+require_relative "./best_netflix_made_movies_gem/cli"
+require_relative "./best_netflix_made_movies_gem/movie"
+require_relative "./best_netflix_made_movies_gem/scraper"
+
 
 module BestNetflixMadeMoviesGem
   # Your code goes here...
