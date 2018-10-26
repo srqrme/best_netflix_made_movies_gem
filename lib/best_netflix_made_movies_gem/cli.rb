8f2f1fd6c035ed4236842a7445fcc3e2a8365ed6
Sharmaine Requerme <srqrme@gmail.com>
2018-10-26 17:04:20 +0000

    added #start and #menu methods to cli.rb

--- lib/best_netflix_made_movies_gem/cli.rb
+++ lib/best_netflix_made_movies_gem/cli.rb
@@ -2,6 +2,7 @@
 require 'pry'
 
 class BestNetflixMadeMoviesGem::CLI
+
   def start
     puts ""
     puts "Hello and Welcome to the Best Netflix Made Movies Gem:"
@@ -10,4 +11,21 @@ class BestNetflixMadeMoviesGem::CLI
     menu
   end
 
+  def menu
+    puts ""
+    puts "What would you like to do? You can:"
+    puts "Enter '1' if you'd like to see Rotten Tomatoes' current list of the best movies made by Netflix, or"
+    puts "Enter '2' to leave the Best Netflix Made Movies Gem"
+    puts ""
+
+    input = gets.strip.to_i
+
+    if input == 1
+      puts ""
+      puts "Here are the top Netflix Original movies, listed in rank order:"
+      puts ""
+      list_movies
+      puts ""
+    end
+  end
 end
