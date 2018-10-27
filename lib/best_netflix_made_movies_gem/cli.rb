8c6771d69b0651ab14f0abc9461caca0c2aca0ea
Sharmaine Requerme <srqrme@gmail.com>
2018-10-27 00:43:37 +0000

    modified cli.rb and scraper.rb files

--- lib/best_netflix_made_movies_gem/cli.rb
+++ lib/best_netflix_made_movies_gem/cli.rb
@@ -8,26 +8,28 @@ class BestNetflixMadeMoviesGem::CLI
     puts "Hello and Welcome to the Best Netflix Made Movies Gem:"
     puts "A guide to the best Netflix made movies according to Rotten Tomatoes"
     puts ""
+
     menu
   end
 
   def menu
-    puts ""
-    puts "What would you like to do? You can:"
-    puts ""
-    puts "Enter '1' if you'd like to see Rotten Tomatoes' current list of the best movies made by Netflix, or"
-    puts "Enter '2' to leave the Best Netflix Made Movies Gem"
+    puts "Would you like to see the full list of movies? Please enter Y/N:"
     puts ""
 
-    input = gets.strip.to_i
+    input = gets.strip
 
-    if input == 1
+    if input == "Y" || input == "y"
       puts ""
       puts "Here are the top Netflix Original movies, listed in rank order:"
       puts ""
       list_movies
       puts ""
       run
+    elsif input == "N" || input == "n"
+      quit
+    else
+      puts "I'm not sure I understand."
+      menu
     end
   end
 
@@ -56,7 +58,7 @@ class BestNetflixMadeMoviesGem::CLI
 
   def view_another
     puts ""
-    puts "Would you like to see the details of another movie? Enter "Y" or "N""
+    puts "Would you like to see the details of another movie? Enter 'Y' or 'N' "
     puts ""
 
     input = gets.chomp
