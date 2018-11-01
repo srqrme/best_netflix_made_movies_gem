ea796fe5fbfc84e0f8928987531ebc18a2f1fb8f
Sharmaine Requerme <srqrme@gmail.com>
2018-11-01 21:52:20 +0000

    added comments to bin/bestNetflixMadeMovies

--- lib/best_netflix_made_movies_gem.rb
+++ lib/best_netflix_made_movies_gem.rb
@@ -2,8 +2,3 @@ require_relative "./best_netflix_made_movies_gem/version"
 require_relative "./best_netflix_made_movies_gem/cli"
 require_relative "./best_netflix_made_movies_gem/movie"
 require_relative "./best_netflix_made_movies_gem/scraper"
-
-
-module BestNetflixMadeMoviesGem
-  # Your code goes here...
-end
